7e4e73d1925994710b857cf23b225a35706ad18d
Haris <haris.amiri@live.com>
2020-12-29 14:31:33 -0800

    replacing home screen with dummy articles

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from flask import Flask
-from flask import jsonify
 
-apiLib = {"articles": [{"url": "https://github.com/",
-                        "imageURL": 'hi',
-                        "logo": 'hi',
-                        "headline": 'Headlines22'},
-                       {"url": "https://google.com/",
-                        "imageURL": "hello",
-                        "logo": "hoo",
-                        "headline": "What's up?"}]}
+apiLib = {"articles": [
+
+    {
+        "headline": "Arsenal to win Premier League in shock Arsene Wenger comeback",
+        'imageURI': 'https://cdn.newsapi.com.au/image/v1/5ce735885933ff71f6943eb9b4b68c1c',
+        'imageLogo': 'https://i.ibb.co/5rBMVXF/BBC-logo.png',
+        'url': "https://www.bbc.com/sport/football/55382725"},
+
+    {
+        "headline": "Whole Foods wins Nobel Prize for solving Climate Change with $60 Dijon Mustard products",
+        "imageURI": "https://m.media-amazon.com/images/S/assets.wholefoodsmarket.com/content/15/e7/47c9886840b299bd24b6e48fca6e/7.Certified-Organic-Grocery-Store_2280x1282._TTW_._CR1,0,2278,1282_._SR1500,844_._QL100_.jpg",
+        "imageLogo": 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/CNN.svg/900px-CNN.svg.png'
+    },
+
+    {
+        "headline": "Fish Fingers, as it turns out, are not made from real fish",
+        "imageURI": 'https://www.irishtimes.com/polopoly_fs/1.3696142.1542101165!/image/image.jpg',
+        "imageLogo": 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/CTV_logo_2018.svg/1200px-CTV_logo_2018.svg.png'
+    }
+
+]}
 
 app = Flask(__name__)
 
@@ -18,8 +30,8 @@ def home():
     return "Fake News API Service"
 
 
-@ app.route('/sports', methods=['GET'])
-def sport():
+@ app.route('/latest', methods=['GET'])
+def latest():
     return apiLib
 
 
